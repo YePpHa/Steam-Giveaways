4db6774edb27de03cc4aaa98ad4e0efb03d0e221
Jackson Roberts <jaxkr@riseup.net>
2014-06-22 18:37:05 -0600

    stuff

---
+++ giveaway.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Steam Giveaways</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="/">Steam Giveaways</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="/">Home</a></li>
+				<li><a href="/newgiveaway.php">New Giveaway</a></li>
+				<li><a href="/donate.html">Donate</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<?php
+		if (isset($_GET['id'])) {
+			$id = $_GET['id'];
+			$db = new mysqli('REDACTED', 'REDACTED', 'REDACTED', 'REDACTED');
+
+			if($db->connect_errno > 0){
+			    die('Unable to connect to database [' . $db->connect_error . ']');
+			}
+			$stmt = $db->prepare('SELECT gametitle, gameplatform, chance, claimed, thankyou, userthankyou, gamekey FROM `keys` WHERE id=?');
+			$stmt->bind_param('i', $id);
+			$stmt->execute();
+			$stmt->store_result();
+			$num_rows = $stmt->num_rows;
+		}
+		if ($num_rows == 0) {
+			echo 'Giveaway not found.';
+			die();
+		}
+
+		$stmt->bind_result($title, $platform, $chance, $claimed, $ty, $uty, $gamekey);
+		$stmt->fetch();
+
+		if ($_POST) {
+			if (!isset($_POST['name'])) {
+
+				require_once('recaptchalib.php');
+				$private_key = 'REDACTED';
+				$resp = recaptcha_check_answer($private_key, $_SERVER['REMOTE_ADDR'], $_POST['recaptcha_challenge_field'], $_POST['recaptcha_response_field']);
+
+				if (!$resp->is_valid) {
+					?>
+					<div class="alert alert-danger">You failed the captcha! Robot scum.</div>
+					<?php
+				} else {
+					$roll = rand(1, $chance);
+					if ($roll == $chance) {
+						?>
+						<div class="alert alert-success">You won! Hooray! Your <?php echo $platform?> game key is <strong><?php echo $gamekey?></strong>.
+							<p>Please leave a note for the person who gifted you the game. This closes the giveaway and is mandatory.
+								<div class="form" style="margin-top: 20px">
+									<form class="form-horizontal" action="" method="POST">
+										<div class="form-group">
+											<label class="col-sm-2 control-label">Your username</label>
+											<div class="col-sm-5">
+												<input name="name" type="text" class="form-control" placeholder="Preferably your reddit one.">
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-sm-2 control-label">Your message</label>
+											<div class="col-sm-5">
+												<textarea class="form-control" name="message"></textarea>
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="col-sm-offset-2 col-sm-10">
+												<button type="submit" class="btn btn-primary">Go!</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+							<?php
+						} else {
+							?>
+							<div class="alert alert-warning">You didn't win. :(<br>Feel free to try again!<br>You rolled a <?php echo $roll?>. You must roll a <?php echo $chance?> to get the key.</div>
+								<?php
+							}
+						}
+					} else {
+						$thankyou = htmlspecialchars($_POST['message']);
+						$userthankyou = htmlspecialchars($_POST['name']);
+						$stmt = $db->prepare('UPDATE `keys` SET claimed=?, thankyou=?, userthankyou=? WHERE id=?');
+						$claimed = true;
+						$stmt->bind_param('issi', $claimed, $thankyou, $userthankyou, $id);
+						$stmt->execute();
+					}
+
+				}
+		?>
+		<div class="well">
+		<?php
+		if ($claimed == 0){
+			?>
+			<p class="lead">This <?php echo $platform?> copy of <strong><?php echo $title?></strong> has not been claimed yet!</p>
+			<p>Fill out this CAPTCHA and click "Go!" for a 1 in <?php echo $chance;?> chance of getting the key! Good luck!</p>
+			<form action="" method="post">
+			<?php
+			require_once('recaptchalib.php');
+			$pubkey = 'REDACTED';
+			echo recaptcha_get_html($pubkey);
+			?>
+			<button class="btn btn-lg btn-primary" style="margin-top: 15px;" type="submit">Go!</button>
+			</form>
+			<?php
+		} else {
+			?>
+			<p class="lead">This <?php echo $platform?> copy of <strong><?php echo $title?></strong> has been claimed!</p>
+			<p>A message from the winner:</p>
+			<blockquote>
+				<p><?php echo $ty?></p>
+				<footer><?php echo $uty?></footer>
+			</blockquote>
+				<?php
+		}
+		?>
+		</div>
+	</div>
+</body>
+</html>
---
+++ newgiveaway.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Steam Giveaways</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<style>
+	.form {margin-top: 40px;}
+	</style>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="/">Steam Giveaways</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="/">Home</a></li>
+				<li class="active"><a href="/newgiveaway.php">New Giveaway</a></li>
+				<li><a href="/donate.html">Donate</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<?php
+		if ($_POST) {
+			$errors = false;
+			if ($_POST['gamename'] !== '' && $_POST['gamekey'] !== '' && $_POST['gameplatform'] !== '' && $_POST['chance'] !== '') {			
+				if (ctype_digit($_POST['chance'])) {
+					if ((int)$_POST['chance'] > 5000) {
+						$errors = true;
+						$message = 'Chance too low.';
+					}
+				} else {
+					$errors = true;
+					$message = 'Chance must be an integer.';
+				}
+			} else {
+				$errors = true;
+				$message = 'You left something blank.';
+			}
+			if ($errors) {
+				?>
+				<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> <?php echo $message;?></div>
+				<?php
+			}
+
+			if (!$errors) {
+				$gamename = htmlspecialchars($_POST['gamename']);
+				$gamekey = htmlspecialchars($_POST['gamekey']);
+				$gameplatform = htmlspecialchars($_POST['gameplatform']);
+				$chance = (int)$_POST['chance'];
+
+				$db = new mysqli('REDACTED', 'REDACTED', 'REDACTED', 'REDACTED');
+
+				if($db->connect_errno > 0){
+				    die('Unable to connect to database [' . $db->connect_error . ']');
+				}
+
+				$stmt = $db->prepare('INSERT INTO `keys` (gamekey, gametitle, gameplatform, chance) VALUES (?,?,?,?)');
+				$stmt->bind_param('sssi', $gamekey, $gamename, $gameplatform, $chance);
+				$stmt->execute();
+				$id = $db->insert_id;
+
+				?>
+				<div class="alert alert-success"><span class="glyphicon glyphicon-ok-sign"></span> Your giveaway has been created.<br>
+					Your public URL is <a href="http://steamgiveaways.jacksonroberts.me/giveaway.php?id=<?php echo $id?>">http://steamgiveaways.jacksonroberts.me/giveaway.php?id=<?php echo $id?></a></div>
+				<?php
+			}
+		}
+		?>
+		<div class="well">
+			<h3>Create a New Giveaway!</h3>
+			<p>Aren't you generous?</p>
+			<div class="form">
+				<form class="form-horizontal" action="" method="POST">
+				  <div class="form-group">
+				    <label class="col-sm-2 control-label">Game Name</label>
+				    <div class="col-sm-8">
+				      <input name="gamename" type="text" class="form-control" placeholder="As it would appear in your game library.">
+				    </div>
+				  </div>
+				  <div class="form-group">
+				  	<label class="col-sm-2 control-label">Game Key</label>
+				  	<div class="col-sm-8">
+				  		<input name="gamekey" type="text" class="form-control" placeholder="N0TR3-ALK3Y-JSTEX">
+				  	</div>
+				  </div>
+				  <div class="form-group">
+				  	<label class="col-sm-2 control-label">Game platform</label>
+				  	<div class="col-sm-5">
+				  		<select class="form-control" name="gameplatform">
+				  			<option value="Steam">Steam</option>
+				  			<option value="Origin">Origin</option>
+				  		</select>
+				  	</div>
+				  </div>
+				  <div class="form-group">
+				  	<label class="col-sm-2 control-label">Chance of giving</label>
+				  	<div class="col-sm-3">
+				  		<div class="input-group">
+				  			<span class="input-group-addon">One out of</span>
+				  			<input class="form-control" type="text" name="chance" value="100">
+				  		</div>
+				  	</div>
+				  </div>
+				  <div class="form-group">
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <button type="submit" class="btn btn-primary">Create!</button>
+				    </div>
+				  </div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
