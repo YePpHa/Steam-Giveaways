bbe0f30a995d328eb5c0483f2f1ffaaad9daa3b8
YePpHa <jepperm@gmail.com>
2014-06-24 15:22:39 +0200

    Using an expanded function for the token generator from http://stackoverflow.com/a/13733588.

--- newgiveaway.php
+++ newgiveaway.php
@@ -1,3 +1,28 @@
+<?php
+function crypto_rand_secure($min, $max) {
+  $range = $max - $min;
+  if ($range < 0) return $min; // not so random...
+  $log = log($range, 2);
+  $bytes = (int) ($log / 8) + 1; // length in bytes
+  $bits = (int) $log + 1; // length in bits
+  $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+  do {
+    $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+    $rnd = $rnd & $filter; // discard irrelevant bits
+  } while ($rnd >= $range);
+  return $min + $rnd;
+}
+
+function getToken($length){
+  $token = "";
+  $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+  for($i=0;$i<$length;$i++){
+    $token .= $codeAlphabet[crypto_rand_secure(0,strlen($codeAlphabet))];
+  }
+  return $token;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -45,8 +70,7 @@
 			}
 
 			if (!$errors) {
-        $strong = true;
-        $token = base64_encode(openssl_random_pseudo_bytes(32, $strong));
+        $token = getToken(64);
 				$gamename = stripslashes(htmlspecialchars($_POST['gamename']));
 				$gamekey = htmlspecialchars($_POST['gamekey']);
 				$gameplatform = htmlspecialchars($_POST['gameplatform']);
